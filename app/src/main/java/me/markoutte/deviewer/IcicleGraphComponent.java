5f890651dc01cef3cdb5d523f857427e88e4fa66
Maksim Pelevin <maks.pelevin@gmail.com>
2025-04-07 22:31:28 +0300

    Double click on frame

--- app/src/main/java/me/markoutte/deviewer/IcicleGraphComponent.java
+++ app/src/main/java/me/markoutte/deviewer/IcicleGraphComponent.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ public class IcicleGraphComponent extends JComponent {
     private int maxDepth = 0;
     private double scale = 1.0;
     private final double FACTOR = 1.01;
+    private Point point = null;
+    private Rectangle hoveredRectangle = null;
 
     public IcicleGraphComponent(StackFrame root, Trie<StackFrame, StackFrame> trie) {
         traverse(trie, trie.getImpl(Collections.singleton(root)), 0.0, 1.0, 0);
@@ -50,6 +54,38 @@ public class IcicleGraphComponent extends JComponent {
                 comp.getParent().dispatchEvent(e);
             }
         });
+        container.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                Point position = container.getViewPosition();
+                point = new Point(
+                        position.x + e.getX(),
+                        position.y + e.getY()
+                );
+                repaint();
+            }
+        });
+        container.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2 && hoveredRectangle != null) {
+                    java.awt.Rectangle rect = container.getViewRect();
+                    scale = 1 / (hoveredRectangle.end - hoveredRectangle.start);
+                    int newWidth = (int) Math.round(rect.width * scale);
+                    int newHeight = 24 * maxDepth;
+                    int newX = (int) Math.round(newWidth * hoveredRectangle.start);
+                    int newY = rect.y;
+                    point = null;
+                    hoveredRectangle = null;
+                    IcicleGraphComponent.this.setPreferredSize(new Dimension(newWidth, newHeight));
+                    container.revalidate();
+                    container.repaint();
+                    SwingUtilities.invokeLater(() -> {
+                        container.setViewPosition(new Point(newX, newY));
+                    });
+                }
+            }
+        });
     }
 
     private void resizeComponent(int x, int y, double scale) {
@@ -60,8 +96,8 @@ public class IcicleGraphComponent extends JComponent {
         );
         int nx = (int) Math.round(scale * (x + viewRect.x) - x);
         parent.setViewPosition(new Point(Math.max(nx, 0), viewRect.y));
-        IcicleGraphComponent.this.revalidate();
-        IcicleGraphComponent.this.repaint();
+        parent.revalidate();
+        parent.repaint();
     }
 
     private void traverse(Trie<StackFrame, StackFrame> trie, Trie.Node<StackFrame> node, double start, double end, int depth) {
@@ -83,6 +119,7 @@ public class IcicleGraphComponent extends JComponent {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, bounds.width, bounds.height);
         Color green = new Color(68, 236, 190);
+        Color greenh = new Color(31, 209, 160);
         Color green2 = new Color(25, 131, 104);
         java.awt.Rectangle rect = getVisibleRect();
         for (Rectangle rectangle : rectangles) {
@@ -93,14 +130,19 @@ public class IcicleGraphComponent extends JComponent {
             if (!rect.intersects(x, y, width, height) || width <= 1) {
                 continue;
             }
+            boolean hovered = point != null && point.x > x && point.y > y && point.x < x + width && point.y < y + height;
             Graphics2D g2 = (Graphics2D) g.create(x, y, width, height);
-            g2.setColor(green);
+            g2.setColor(!hovered ? green : greenh);
             g2.fillRect(0, 0, width, height);
             g2.setColor(green2);
             g2.drawRect(0, 0, width, height);
             g2.setColor(Color.BLACK);
-            g2.drawString(rectangle.frame.methodName(), 5, 16);
+            int max = Math.max(0, rect.x - x);
+            g2.drawString(rectangle.frame.methodName(), max + 5, 16);
             g2.dispose();
+            if (hovered) {
+                hoveredRectangle = rectangle;
+            }
         }
     }
 
